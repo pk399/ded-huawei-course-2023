ba35892dac250b7d0a3ba2f265c6e385eb566c84
pk399 <nikkiter2@mail.ru>
2023-09-15 21:07:39 +0300

    Day 3 (actually 5): Eugene Onegin initial working version! :)

---
+++ day 3/text5.cpp
@@ -0,0 +1,313 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+const size_t LN_WIDTH = 3;
+
+
+enum ORDER {
+    OK = 0,
+    SWAP = 1,
+};
+
+
+struct text5 {
+    char** lines;
+    size_t nLines;
+    char* chars;
+    size_t nChars;
+};
+
+
+int FileSize(size_t* sz, const char* path)
+{
+    struct stat stats_buf = {};
+
+    if (stat(path, &stats_buf))
+        return -1;
+
+    *sz = stats_buf.st_size;
+    //printf("Size: %zd\n", *sz);
+    return 0;
+}
+
+
+void FindLines(text5* txt)
+{
+    txt->lines[0] = txt->chars;
+
+    size_t nline = 1;
+    for (size_t i = 0; i < txt->nChars; i++)
+        if (txt->chars[i] == '\n')
+        {
+            txt->chars[i] = '\0';
+            txt->lines[nline++] = txt->chars + i + 1;
+        }
+
+    txt->nLines = nline;
+}
+
+
+int ReadFromFile(text5* txt, const char* path)
+{
+    size_t sz = 0;
+    if (FileSize(&sz, path)) return -1;
+
+    sz++;
+    txt->nChars = sz;
+    txt->chars = (char*) calloc(sz, sizeof(char));
+    if (!txt->chars) return -1;
+
+
+    FILE* file = fopen(path, "rb");
+    if (!file) return -1;
+    size_t sz_read = fread(txt->chars, sizeof(char), sz, file);
+
+
+    /*perror("errno");
+    printf("Read: %zd, stat: %zd\n", sz_read, sz);
+    fclose(file);
+
+    printf("Last chars:");
+    for (int i = 0 ; i< 10000; i++)
+        printf("[%c %d]", txt->chars[sz-i], txt->chars[sz-i]);
+    putchar('\n');*/
+
+
+    const size_t MAX_LINES = 60015;
+    txt->lines = (char**) calloc(MAX_LINES, sizeof(char*));
+    if (!txt->lines) return -1;
+    FindLines(txt);
+
+    return 0;
+}
+
+
+bool HasAlpha(const char* str)
+{
+    for (size_t i = 0; str[i]; i++)
+        if (isalpha(str[i]))
+            return true;
+
+    return false;
+}
+
+
+void PrintSampleAlpha(const text5* txt, size_t lines)
+{
+    size_t printed = 0;
+
+    for (size_t i = 0; i < txt->nLines; i++)
+        if (HasAlpha(txt->lines[i]))
+        {
+            printed++;
+
+            printf("%*zd: %s\n",
+                   LN_WIDTH, printed,
+                   /*(i != txt->nLines - 1) ?
+                   (int) (txt->lines[i+1] - txt->lines[i] - 1) :
+                   strlen(txt->lines[i]),*/ txt->lines[i]);
+
+            if  (printed == lines)
+                break;
+        }
+}
+
+
+void PrintSample(const text5* txt, size_t lines)
+{
+    for (size_t i = 0; i < lines && i < txt->nLines; i++)
+        printf("%*zd: %s\n",
+               LN_WIDTH, i + 1,
+               /*(i != txt->nLines - 1) ?
+               (int) (txt->lines[i+1] - txt->lines[i] - 1) :
+               strlen(txt->lines[i]),*/ txt->lines[i]);
+}
+
+
+void TextKiller(text5* txt)
+{
+    free(txt->chars);
+    txt->chars = 0;
+    txt->nChars = 0;
+
+    free(txt->lines);
+    txt->lines = 0;
+    txt->nLines = 0;
+}
+
+void Print(const text5* txt)
+{
+    PrintSample(txt, (size_t) -1);
+}
+
+
+void* VoidIndex(const void* arr, size_t el_size, size_t index)
+{
+    assert(arr);
+    assert(el_size);
+
+    return (void*) ((size_t) arr + index*el_size);
+}
+
+
+void VoidSwap(void* arr, size_t el_size, size_t idx1, size_t idx2)
+{
+    assert(arr);
+    assert(el_size);
+
+    char temp = 0;
+
+    char* el1 = (char*) VoidIndex(arr, el_size, idx1);
+    char* el2 = (char*) VoidIndex(arr, el_size, idx2);
+
+    for (size_t i = 0; i < el_size; i++)
+    {
+        temp = el1[i];
+        el1[i] = el2[i];
+        el2[i] = temp;
+    }
+}
+
+
+void BubbleSort(void* arr, size_t el_size, size_t len,
+                ORDER (*cmp)(const void*, const void*))
+{
+    assert(arr);
+    assert(cmp);
+    assert(el_size);
+
+    if (len < 2) return;
+
+    for (size_t i = 0; i < len - 1; i++)
+    {
+        bool wasSwapped = false;
+
+        for (size_t j = 0; j < len - i - 1; j++)
+            if (cmp(VoidIndex(arr, el_size, j), VoidIndex(arr, el_size, j+1))
+                == SWAP)
+            {
+                wasSwapped = true;
+                VoidSwap(arr, el_size, j, j+1);
+            }
+
+        if (!wasSwapped)
+            break;
+    }
+}
+
+
+bool Myisalpha(char a)
+{
+    //a:97 z:122 A:65 Z:90
+    return ((a >= 65) && (a <= 90)) || ((a >= 97) && (a <= 122));
+}
+
+
+ORDER StrCompL2R(const void* a_void, const void* b_void)
+{
+    assert(a_void);
+    assert(b_void);
+
+    char* a = *((char**) a_void);
+    char* b = *((char**) b_void);
+
+    for (size_t i = 0, j = 0; a[i] && b[j]; i++, j++)
+    {
+        while (!isalpha(a[i]) && a[i]) i++;
+        while (!isalpha(b[j]) && b[j]) j++;
+
+        if (tolower(a[i]) > tolower(b[j]))
+            return SWAP;
+        else if (tolower(a[i]) < tolower(b[j]))
+            return OK;
+    }
+
+    return OK;
+}
+
+
+void SortL2R(text5* txt)
+{
+    BubbleSort(txt->lines, sizeof(char*), txt->nLines, &StrCompL2R);
+}
+
+
+ORDER StrCompR2L(const void* a_void, const void* b_void)
+{
+    assert(a_void);
+    assert(b_void);
+
+    char* a = *((char**) a_void);
+    char* b = *((char**) b_void);
+
+    for (int i = strlen(a) - 1, j = strlen(b) - 1; i > 0 && j > 0; i--, j--)
+    {
+        while (!isalpha(a[i]) && i > 0) i--;
+        while (!isalpha(b[j]) && j > 0) j--;
+
+        if (i < 0 || j < 0)
+            return SWAP;
+
+
+        if (tolower(a[i]) > tolower(b[j]))
+            return SWAP;
+        else if (tolower(a[i]) < tolower(b[j]))
+            return OK;
+    }
+
+    return OK;
+}
+
+
+void SortR2L(text5* txt)
+{
+    BubbleSort(txt->lines, sizeof(char*), txt->nLines, &StrCompR2L);
+}
+
+
+ORDER s1mple_cmp(const void* a, const void* b)
+{
+    assert(a);
+    assert(b);
+
+    return (*(long long unsigned*)a > *(long long unsigned*)b) ? SWAP : OK;
+}
+
+
+int main(void)
+{
+    /*long long unsigned l[5] = {133333333337, 420, 3, 1, 2};
+    printf("%llu %llu %llu %llu %llu\n", l[0], l[1], l[2], l[3], l[4]);
+    BubbleSort(l, sizeof(l[0]), 5, &s1mple_cmp);
+    printf("%llu %llu %llu %llu %llu\n", l[0], l[1], l[2], l[3], l[4]);*/
+
+
+    const char* PUSHKIN = "evgenij_onegin_en.txt";
+
+    text5 evgenij_onegin = {};
+    int result = ReadFromFile(&evgenij_onegin, PUSHKIN);
+    assert(!result);
+
+
+    printf("\n======   NORMAL ORDER   ======\n");
+    PrintSample(&evgenij_onegin, 30);
+
+    printf("\n======   SORT L2R   ======\n");
+    SortL2R(&evgenij_onegin);
+    PrintSampleAlpha(&evgenij_onegin, 30);
+
+    printf("\n======   SORT R2L   ======\n");
+    SortR2L(&evgenij_onegin);
+    PrintSampleAlpha(&evgenij_onegin, 30);
+
+    TextKiller(&evgenij_onegin);
+}
