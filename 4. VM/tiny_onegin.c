0560c90a055bb188f5e2ab8b8d91a73a78f6d40a
pk399 <nikkiter2@mail.ru>
2023-10-20 09:30:42 +0300

    4: tiny_onegin now based on memory.h

--- 4. VM/tiny_onegin.c
+++ 4. VM/tiny_onegin.c
@@ -5,7 +5,19 @@
 #include <assert.h>
 #include <stdlib.h>
 
+#include "memory.h"
+#include "error.h"
+
 #include "tiny_onegin.h"
+
+
+const unsigned DUMP_LIMIT = 50;
+
+
+struct text6 {
+	Memory* buf;
+	Memory* lines;
+};
 
 
 unsigned _get_file_size(FILE* stream) {
@@ -18,34 +30,142 @@ unsigned _get_file_size(FILE* stream) {
 
 
 Text* TextCtor(FILE* file) {
-	unsigned f_sz = _get_file_size(file);
-	char* buf = (char*) calloc(1, f_sz + 1);
-	assert(buf);
-	
-	unsigned read = fread(buf, 1, f_sz, file);
-	assert(read == f_sz);
+	assert(file);
 	
 	Text* txt = (Text*) calloc(1, sizeof(Text));
-	assert(txt);
+	if (!txt) {
+		FATAL("Calloc returned NULL");
+		return NULL;
+	}
 	
-	txt->buf = buf;
+	if ( !(txt->buf = MemCtor(sizeof(char))) ) {
+		FATAL("Error constructing memory for chars");
+		free(txt);
+		return NULL;
+	}
 	
-	unsigned lp = 0;
-	txt->lines[lp++] = buf;
-	for (unsigned i = 0; buf[i]; i++)
-		if (buf[i] == '\n') {
-			if (buf[i+1])
-				txt->lines[lp++] = buf + i + 1;
-			buf[i] = '\0';	
+	unsigned f_sz = _get_file_size(file);
+	if ( MemResize(txt->buf, f_sz + 1) ) { // <---- F_SZ + 1
+		FATAL("Error resizing memory");
+		MemDtor(txt->buf);
+		free(txt);
+		return NULL;
+	}
+	
+	unsigned read = fread(TextChars(txt), 1, f_sz, file);
+	if (read != f_sz) {
+		FATAL("Read less bytes than filesize");
+		MemDtor(txt->buf);
+		free(txt);
+		return NULL;
+	}
+	
+	if ( !(txt->lines = MemCtor(sizeof(char*))) ) {
+		FATAL("Error constructing memory for lines");
+		MemDtor(txt->buf);
+		free(txt);
+		return NULL;
+	}
+	
+	char* tmp = TextChars(txt);
+	MemWrite(txt->lines, &tmp);
+	MemShift(txt->lines, 1);
+	
+	printf(BLUE("%s\n"), TextChars(txt));
+	MemDump(txt->buf);
+	
+	for (unsigned j = 0 ; !MemEOF(txt->buf) && j < 150 ; MemShift(txt->buf, 1), j ++) {
+		// printf("BIG LLOP %u %d\n", MemGetPt(txt->buf), MemEOF(txt->buf));
+		// MemDump(txt->buf);
+		char c = 0;
+		MemRead(txt->buf, &c);
+		if ( c == '\n' && TextChars(txt)[MemGetPt(txt->buf) + 1] ) {
+			MemZero(txt->buf); // \n --> \0
+			tmp = TextChars(txt) + MemGetPt(txt->buf) + 1;
+			MemWrite(txt->lines, &tmp);
+			MemShift(txt->lines, 1);
 		}
+	}
+	
+	if ( MemResize(txt->lines, MemGetPt(txt->lines)) ) {
+		FATAL("Error resizing lines");
+		TextDtor(txt);
+		return NULL;
+	}
 	
 	return txt;
 }
 
+
+int TextDump(Text* txt) {
+	assert(txt);
+	assert(txt->buf);
+	assert(txt->lines);
 	
-int TextDtor(Text* txt) {
-	free(txt->buf);
-	free(txt);
+	printf(GREEN("Text") "[" BLUE("%p") "] {\n", txt);
+	
+	printf( "\t" GREEN("Buffer") " - " );
+	MemDump(txt->buf);
+	
+	printf( "\t" GREEN("Lines") " - " );
+	MemDump(txt->lines);
+	
+	printf("\n");
+	
+	char* line = 0;
+	for (unsigned i = 0; i < DUMP_LIMIT && ( line = TextGetLine(txt, i) ); i++)
+		printf( "\t" YELLOW("%3u") " [" BLUE("%3ld") "]: %s\n", i, line - TextChars(txt), line);
+	 
+	printf("}\n");
 	
 	return 0;
+}
+
+	
+int TextDtor(Text* txt) {
+	if (txt) {
+		MemDtor(txt->buf);
+		MemDtor(txt->lines);
+		free(txt);
+	}
+	
+	return 0;
+}
+
+
+char* TextGetLine(Text* txt, unsigned index) {
+	assert(txt);
+	assert(txt->buf);
+	assert(txt->lines);
+	
+	if ( MemEOFPt(txt->lines, index) )
+		return NULL;
+	
+	return TextLines(txt)[index];
+}
+
+
+char* TextChars(Text* txt) {
+	assert(txt);
+	assert(txt->buf);
+	
+	return (char*) MemGetBf(txt->buf);
+}
+
+
+char** TextLines(Text* txt) {
+	assert(txt);
+	assert(txt->buf);
+	assert(txt->lines);
+	
+	return (char**) MemGetBf(txt->lines);
+}
+
+
+unsigned TextLinesLen(Text* txt) {
+	assert(txt);
+	assert(txt->buf);
+	assert(txt->lines);
+	
+	return (unsigned) MemGetSz(txt->lines);
 }
--- 4. VM/tiny_onegin.h
+++ 4. VM/tiny_onegin.h
@@ -3,15 +3,17 @@
 
 #include <stdio.h>
 
-struct text6 {
-	char* buf;
-	char* lines[65536];
-};
-
 typedef struct text6 Text;
 
 Text* TextCtor(FILE*);
+int TextDump(Text*);
 int TextDtor(Text*);
 
+char* TextGetLine(Text*, unsigned index);
+
+// Getters
+char* TextChars(Text*);
+char** TextLines(Text*);
+unsigned TextLinesLen(Text*);
 
 #endif /* _TINY_ONEGIN_H_ */
