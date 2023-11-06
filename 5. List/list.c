4ffb0c2d32ad2b0c14e01f5956a0d3a52e264245
pk399 <nikkiter2@mail.ru>
2023-11-06 19:48:22 +0300

    5: graphic dump

--- 5. List/list.c
+++ 5. List/list.c
@@ -31,6 +31,8 @@ List* ListCtor(int capacity);
 int ListResizeUp(List* lst, int new_cap);
 int ListValidate(const List* lst);
 int ListTextDump(const List* lst);
+int ListGraphDump(const List* lst);
+//int ListGraphTemplate(const char* filename, const List* lst, const char* template);
 int ListDtor(List* lst);
 
 
@@ -156,6 +158,57 @@ int ListTextDump(const List* lst) {
 }
 
 
+int ListGraphDump(const List* lst) {
+    // TODO: this -> int GetTime(char* buf)
+    time_t t = time(NULL);
+    assert(t != -1);
+    struct tm* lt = localtime(&t);
+    
+    const unsigned MX_FILENAME_LEN = 32;
+    char timestr[MX_FILENAME_LEN] = {};
+    strftime(timestr, MX_FILENAME_LEN, "%Y-%m-%d %H:%M:%S", lt);
+    // ------------------------------------
+    
+    //ListGraphTemplate(fn, lst, "template1.dot"); Doing it manually
+    
+    char strbuf[MX_FILENAME_LEN + 100] = "";
+    
+    sprintf(strbuf, "dot -Tpng -o \"Dump %s.png\"", timestr);
+    
+    FILE* f = popen(strbuf, "w");
+    //FILE* f = fopen("test.dot", "wt");
+    if (!f) return -1;
+    
+    fprintf(f, "digraph {\n rankdir=LR;\n subgraph cluster {\nstyle=filled;\n color=lightgrey;\n label=\"List ultra mega\"\n");
+    
+    for (int i = 0; i < lst->capacity; i++) {
+        fprintf(f, " n%d[shape=\"Mrecord\", label=\"%d | <n> next(%d) | <p> prev(%d)\" ", i, i, lst->next[i], lst->prev[i]);
+        if (i == 0) fprintf(f, "color=\"purple\"");
+        else if (lst->prev[i] == -1) fprintf(f, "color=\"lime\"");
+        fprintf(f, "]\n");
+    }
+    
+    
+    fprintf(f, "} n0");
+    for (int i = 1; i < lst->capacity; i++) {
+        fprintf(f, " -> n%d", i);
+    }
+    fprintf(f, "[weight=\"69420236\", color=\"#00000000\"];\n");
+    
+    for (int i = 0; i < lst->capacity; i++) {
+        if (lst->prev[i] >= 0)
+            fprintf(f, "n%d: <p> -> n%d [color=\"red\"];\n", i, lst->prev[i]);
+        if (lst->next[i] >= 0)
+            fprintf(f, "n%d: <n> -> n%d [color=\"blue\"];\n", i, lst->next[i]);
+    }
+    
+    fprintf(f, "}\n");
+    
+    pclose(f);
+    return 0;
+}
+
+
 int ListDtor(List* lst) {
     if (lst) {
         if (lst->data) free(lst->data);
@@ -171,10 +224,18 @@ int ListDtor(List* lst) {
 }
 
 
+int MyPrintf() {
+    printf("213\n");
+    return 0;
+}
+
+
 int main() {
+    //ListGraphDump(NULL);
     List* lst = ListCtor(16);
     
-    ListTextDump(lst);
+    //ListTextDump(lst);
+    ListGraphDump(lst);
     
     ListDtor(lst);
 }
